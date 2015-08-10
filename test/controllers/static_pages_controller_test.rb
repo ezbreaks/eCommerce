566637cb6174b2c0f351dd2c08e671546133b182
Chirvase Dragos <chirvase.dragos.andrei@gmail.com>
2015-08-10 21:44:55 +0300

    New Static Pages Begin

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', 'Home | eCommerce Website'
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select 'title', 'About | eCommerce Website'
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select 'title', 'Contact | eCommerce Website'
   end
 
 end
