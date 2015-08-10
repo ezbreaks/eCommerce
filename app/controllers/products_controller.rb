ecc17a0c678fca77e58574e02ae2ff9b62ac57d9
Chirvase Dragos <chirvase.dragos.andrei@gmail.com>
2015-08-11 01:36:26 +0300

    setup paperclip for image

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
 	end
 
 	def product_params
-		params.require(:product).permit(:title, :link, :description)
+		params.require(:product).permit(:title, :link, :description, :image)
 	end
 end
