73f9b8ba3952517bf8e2e61428a61931226dcd4a
Chirvase Dragos <chirvase.dragos.andrei@gmail.com>
2015-08-14 22:02:46 +0300

    Revert "Add Twitter and Incrementations to the order item"
    This reverts commit 7b96838fe1e7a7110e8119f6f10b616a43b1b962.

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   resources :products
-  
-  match '/auth/:provider/callback', to: 'sessions#create', via: :get
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
